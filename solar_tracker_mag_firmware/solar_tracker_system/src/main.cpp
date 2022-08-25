58f43fbc4ae866a5b69da0c417d9781754b5ad85
Robert Mutura <robertmutura90@gmail.com>
2022-08-25 10:33:30 +0300

    updates on the loop

--- solar_tracker_mag_firmware/solar_tracker_system/src/main.cpp
+++ solar_tracker_mag_firmware/solar_tracker_system/src/main.cpp
@@ -6,9 +6,23 @@
 #include <WiFi.h>
 #include<ezTime.h>
 #include<math.h>
+#include <AccelStepper.h>
+#include <EEPROM.h>
+
+#define EEPROM_SIZE 1
 
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Define pin connections
+const int dirPin = 19;
+const int stepPin = 18;
+
+int angular_pos = 0;
+
+#define motorInterfaceType 1
+
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
 
 RTC_DS1307 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
@@ -24,23 +38,44 @@ const double pi = 3.14159265358979323846;
 
 #define red_led   16
 #define green_led 17
-
+#define channel_A 12
+#define channel_B 13
+#define set_pin 35
+//encoder type: https://eu.mouser.com/ProductDetail/Broadcom-Avago/HEDS-5505I06?qs=RuhU64sK2%252Bs%2FooH6iJf%2FsQ%3D%3D
 /*************************/
 int set_flag = 0;
-void set_angle ();
-
+int encoder_pulses = 0;
 
 // time keeping variables
 long current_time = 0;
 long minute_counts = 0;
 long minute_length = 60000;
+
+float get_solar_altitude();
+void set_angle ();
+
+void IRAM_ATTR encoder(){
+      if(!digitalRead(channel_B)){
+        //IF A is falling and B is low then incement the count
+          encoder_pulses++;
+      }
+     else if(digitalRead(channel_B)) {
+        //a is falling and and B is high then decrement the count
+         encoder_pulses--;
+     }
+}
+
 void setup() {
   // put your setup code here, to run once:
      Serial.begin(115200);
      pinMode (red_led, OUTPUT  );
      pinMode (green_led, OUTPUT);
+
      initialize_mag();
-    
+     
+     EEPROM.begin(EEPROM_SIZE);
+     //read the content of eeprom
+     angular_pos = EEPROM.read(0);
     if (! rtc.begin()) {
           Serial.println("Couldn't find RTC do not proceed");
           while (1);
@@ -70,23 +105,56 @@ void setup() {
       	
       }*/ // TODO: INTEGRATE NTP SERVER TO SET TIME OF THE RTC ACCURATELY WITH LOCATION USING ezTIME
 
-    
-    current_time = millis();
+      attachInterrupt(channel_A, encoder, FALLING);
+      /********stepper motor setting************/
+      	myStepper.setMaxSpeed(1000);
+        myStepper.setAcceleration(50);
+      	myStepper.setSpeed(200);
+
+      current_time = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    if(!digitalRead(set_pin)){
+      set_flag = 1;
+       set_angle();
+    }
+    else{
+      set_flag = 0;
+    }
+  
     if(millis() - current_time > minute_length)//if the millis reach i minute count
     {
       minute_counts++;
+      current_time = millis();
     }
 
     if (minute_counts>= 60)//if we get to 1hr then proceed to do this
     {
+
         //TODO : READ ENCODER TO GET THE ANGULAR POSITION OF THE STEPPER
+         int current_angle = EEPROM.read(0);
+         debug("current angle = ");
+         debugln(current_angle);
+
         // TODO: GET THE NEW ANGLE
+        float new_angle = get_solar_altitude(); //this is the new angle to move to
+        debug("New angle = ");
+        debugln(new_angle);
         //TODO: MOVE THE STEPPER MOTOR TO THE NEW ANGLE
-      current_time = millis();
+        myStepper.moveTo(long (new_angle/0.225));//the variable in move to is absolute from 0  -- 0.225 is the degrees moved per step -- step resulution = 1/8(1600steps/rev)
+        myStepper.run();
+        debug("MOVED TO = ");
+        debugln((new_angle/0.225));
+        minute_counts  =  0;
+        angular_pos = (encoder_pulses*0.3515625); //get the current abgle as read by the encoder
+        debug("New anglar position = ");
+        debugln(angular_pos);
+        //save to eeprom
+        EEPROM.write(0 , angular_pos);
+        EEPROM.commit();
+        //NEW ANGLE SAVED TO EEPROM
     }
 }
 
@@ -95,13 +163,21 @@ void set_angle(){
   //select set button to enter this mode
    while(set_flag)
    {
-        int compass_angle = get_angle();
-        if (compass_angle == 0)
+        int compass_angle = get_angle();//obtain the zineth angle of the compass
+        if (compass_angle == 0)//check that the angle is zero
           {
+            /*while in set mode reposition the solar pannel(stepper motor to the zero position and press the boot button 
+            once to set the position to zero.*/
+            
+            //ensure that the stepper motor is zero
+            encoder_pulses = 0;//reset the stepper angle to zero
+                                 //reset myStepper angle to zero as well.
+            myStepper.setCurrentPosition(0); //set the current position of the stepper motor to zero
+           
             digitalWrite(green_led, HIGH);
             digitalWrite(red_led, LOW);
-             debugln (NORTH FOUND!!);
-
+            debugln (NORTH FOUND!!);
+            
             break;
           }
         else
@@ -109,6 +185,7 @@ void set_angle(){
                digitalWrite(red_led, HIGH);
                digitalWrite(green_led, LOW);
                debugln (please locate the north);
+               break;
             }
            
    }
@@ -116,9 +193,6 @@ void set_angle(){
 
 
 /**************************************************************************/
-float get_solar_angle(){
-      
-}
 
 /**************************************************************************/
 int get_days (int month, int day){
@@ -202,7 +276,8 @@ float hour_angle(){
 }
 
 float get_solar_altitude(){
-     int y = sin(latitude * pi/180 );
+
+     int y = sin(latitude *pi/180 );
      y = y*sin(declination_angle()* (pi/180));
      int x = cos(latitude *pi/180 ) *(cos(hour_angle()) * pi/180 );//we get int x in radians
     
@@ -213,3 +288,4 @@ float get_solar_altitude(){
     angle = (angle *  180/pi) ;
     return angle;
     }
+
